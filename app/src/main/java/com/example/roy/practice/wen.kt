aa938d8c6c158b2b2166d1493972c78366b8d893
wenkuuk <405410969@s05.tku.edu.tw>
2018-03-06 22:11:09 +0800

    Initial commit

---
+++ app/src/main/java/com/example/roy/practice/wen.kt
@@ -0,0 +1,30 @@
+package com.example.roy.practice
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_wen.*
+
+class wen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wen)
+
+        // Example of a call to a native method
+        sample_text.text = stringFromJNI()
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+
+        // Used to load the 'native-lib' library on application startup.
+        init {
+            System.loadLibrary("native-lib")
+        }
+    }
+}
